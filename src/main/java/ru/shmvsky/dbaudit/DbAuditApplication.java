0f480ceae3a6b53f90a44515dd88bb5b94ee53a8
shmvsky <grrenshi1337@gmail.com>
2022-08-19 00:25:49 +0300

    initial

---
+++ src/main/java/ru/shmvsky/dbaudit/DbAuditApplication.java
@@ -0,0 +1,13 @@
+package ru.shmvsky.dbaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbAuditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/shmvsky/dbaudit/DbAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shmvsky.dbaudit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
