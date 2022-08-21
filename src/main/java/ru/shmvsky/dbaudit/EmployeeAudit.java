0bbbde08973ad3ff1fe99e9f1c973f2ef0cb382d
shmvsky <grrenshi1337@gmail.com>
2022-08-21 22:42:23 +0300

    audit is working!

---
+++ src/main/java/ru/shmvsky/dbaudit/EmployeeAudit.java
@@ -0,0 +1,50 @@
+package ru.shmvsky.dbaudit;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt","updatedAt"},allowGetters = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeAudit implements Serializable {
+	
+	@CreatedBy
+	@Column(name = "created_by", nullable = false, updatable = false)
+	private String createdBy;
+
+	@CreatedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private Date createdAt;
+
+	@LastModifiedBy
+	@Column(name = "updated_by", nullable = false)
+	private String updatedBy;
+
+	@LastModifiedDate
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "updated_at", nullable = false)
+	private Date updatedAt;
+
+}
---
+++ src/main/java/ru/shmvsky/dbaudit/EmployeeController.java
@@ -0,0 +1,49 @@
+package ru.shmvsky.dbaudit;
+
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+	
+	private EmployeeRepository employeeRepository;
+
+	public EmployeeController(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Employee> getEmployee(@PathVariable Long id) {
+		Optional<Employee> employee = employeeRepository.findById(id);
+
+		if (employee.isPresent()) {
+			return ResponseEntity.status(HttpStatus.OK).body(employee.get());
+		}
+
+		throw new EmployeeNotFoundException("NIGGER");
+
+	}
+
+	@PostMapping
+	public ResponseEntity<Employee> saveEmployee(@RequestParam String name, @RequestParam Integer age) {
+		
+		Employee employee = Employee.builder()
+			.name(name)
+			.age(age)
+			.build();
+		
+		Employee persistedEmployee = employeeRepository.save(employee);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(persistedEmployee);
+	}
+
+}
